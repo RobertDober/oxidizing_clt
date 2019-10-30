c3e653012a1aa936b536135b9ffba5322d264e5b
RobertDober <robert.dober@gmail.com>
2019-10-30 10:30:26 +0100

    Scaffolded main

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
+use structopt::StructOpt;
+
+#[derive(StructOpt)]
+struct Cli {
+    subcommand: String,
+    prefix: Option<String>
+
+}
 fn main() {
-    println!("Hello, world!");
+    let args = Cli::from_args();
+    match args.subcommand.as_str() {
+        "randomize"                => println!("Not yet implemented"),
+        "rename-for-unix"          => println!("Coming soon"),
+        "mv-with-prefix-and-count" => println!("moving"),
+        subcommand                 => panic!("unknown subcommand {}", subcommand)
+    }
 }
